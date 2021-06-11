4197d0865fa7f81627e215ee9b8938926ff19162
vitor <vesteves33@gmail.com>
2021-06-11 13:12:50 -0300

    fazendo um hello world com o streamlit

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+#Fazendo o famoso Hello World no Streamlit --- as strings utilizam regras de Markdown
+
+st.write("""
+# My first data-app           
+**Hello** *world*
+""")
+
